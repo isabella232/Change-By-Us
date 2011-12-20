dc858a336391bfe1ef38ef217d8c223fd312477e
Zach Williams <hey@zachwill.com>
2011-12-20 12:38:52 -0800

    Add icanhaz template and begin adding CSS styles

--- static/js/pages/project.wayfinding.js
+++ static/js/pages/project.wayfinding.js
@@ -6,19 +6,28 @@
 var tc = tc || {};
 tc.gam = tc.gam || {};
 tc.gam.project_widgets = tc.gam.project_widgets || {};
+tc.gam.wayfinding = tc.gam.wayfinding || {};
 
 tc.gam.project_widgets.wayfinding = function(options) {
-    tc.util.log('wayfinding...');
+    tc.util.log('project.wayfinding');
     var dom = options.dom,
-        self = {};
+        self = {},
+        west = tc.jQ('.continent.project').find('.west:last');
 
     tc.jQ(tc).bind('show-project-widget', function(event, widgetName) {
         if (options.name === widgetName) {
+            var contents, template;
             tc.util.log('&&& showing ' + options.name);
             tc.showProjectWidget(dom);
+            contents = west.contents().remove();
+            tc.gam.wayfinding.west = contents;
+            template = ich.west_wayfinding();
+            west.html(template);
         } else {
             tc.util.log('&&& hiding ' + options.name);
             dom.hide();
+            west.html(tc.gam.wayfinding.west);
+            delete tc.gam.wayfinding.west;
         }
     });
 

8e5d6dbba3cb683291b61d22e70e42fd269eac17
Zach Williams <hey@zachwill.com>
2012-04-17 17:35:24 -0700

    Begin adding apply page

--- controllers/grants.py
+++ controllers/grants.py
@@ -5,4 +5,7 @@ class Grants(Controller):
 
     def GET(self, action=None, param0=None):
         """Process a GET request to the grants page."""
+        print action
+        if (action == 'apply'):
+            return self.render('apply')
         return self.render('grants')
